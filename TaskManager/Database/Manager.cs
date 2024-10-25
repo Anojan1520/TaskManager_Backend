563fb978e793861c9b21f5cc72b83b98e630d616
Anojan1520 <unicomtic1520@gmail.com>
2024-10-25 20:17:09 +0530

    First commit

---
+++ TaskManager/Database/Manager.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManager.Models;
+
+namespace TaskManager.Database
+{
+    public class Manager : DbContext
+    {
+        public Manager(DbContextOptions<Manager> Options) : base(Options) 
+        {
+            
+        }
+        public DbSet<TaskItem> Tasks { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ TaskManager/Models/TaskItem.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualBasic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskManager.Models
+{
+    public class TaskItem
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public DateTime Duedate { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Priority { get; set; }
+    } 
+}
+
+
+
+
+
